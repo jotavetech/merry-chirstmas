3831b5cafd70eb4a8e6b5706f3f22653b4be317e
jotavetech <ojoaogugel@gmail.com>
2023-12-24 20:52:10 -0300

    felis natal 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,68 @@ function App() {
 
   const [visible, setVisible] = useState(false);
 
+  const [invite, setInvite] = useState("");
+
+  const [link, setLink] = useState("");
+
   const handleSurprise = () => {
     setVisible(true);
   };
 
+  const createLink = () => {
+    setLink(`felisnatal.vercel.app/${invite}`);
+  };
+
   return (
     <div>
-      {username && (
+      {!username && (
+        <div>
+          MANDE UM PRESENTE PARA ALGUEM
+          <br />
+          <input
+            value={invite}
+            onChange={({ target }) => setInvite(target.value)}
+            type="text"
+            placeholder="PRIMEIRO NOME DA PESSOA"
+            style={{ width: 250, height: 30, marginTop: 15, fontSize: 16 }}
+          />
+          <br />
+          <button disabled={!invite} onClick={createLink}>
+            CRIAR LINK
+          </button>
+          {link && <p>{link}</p>}
+        </div>
+      )}
+      {username && !visible && (
         <button onClick={handleSurprise}>
           CLIQUE AQUI PARA GANHAR UMA SURPRESA
         </button>
       )}
       {visible && (
-        <p>
-          FELIs NATAL {username.toUpperCase()} !!!!!!!!!!!!
-          🥰🥰🥰🤩🤩🤩👏👏👏👏👏👏💯💯💯💯💋💋💋💋💋💓💓💓💓💓💓💓🌲🌲🌲🌲🌲🌲
-        </p>
+        <>
+          <p>
+            FELIs NATAL {username.toUpperCase()} !!!!!!!!!!!!
+            🥰🥰🥰🤩🤩🤩👏👏👏👏👏👏💯💯💯💯💋💋💋💋💋💓💓💓💓💓💓💓🌲🌲🌲🌲🌲🌲
+          </p>
+          <br />
+
+          <div>
+            AGORA MANDE PARA ALGUEM TAMBEM
+            <br />
+            <input
+              value={invite}
+              onChange={({ target }) => setInvite(target.value)}
+              type="text"
+              placeholder="PRIMEIRO NOME DA PESSOA"
+              style={{ width: 250, height: 30, marginTop: 15, fontSize: 16 }}
+            />
+            <br />
+            <button disabled={!invite} onClick={createLink}>
+              CRIAR LINK
+            </button>
+            {link && <p>{link}</p>}
+          </div>
+        </>
       )}
     </div>
   );
